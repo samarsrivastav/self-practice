097ecc7a34823c888779de583be6d5b3c501a323
samar srivastav <samarsrivastav69@gmail.com>
2022-07-03 19:10:25 +0530

    Create CFRTEST.cpp

---
+++ CFRTEST.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[50]={};
+       for(int i=0;i<n;i++){
+        cin>>arr[i];
+       }
+       sort(arr,arr+n);
+       int count=0;
+       for(int i=0;i<n;i++){
+        if(arr[i]!=arr[i+1]){
+            count++;
+        }
+       }
+       cout<<count<<endl;
+    }
+}
